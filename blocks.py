5936b831ac221bebb3d5b88b9affceaeec8c3b9c
Alex Atkins <alex04634@gmail.com>
2016-09-29 03:07:08 -0400

    model file. Added new elements and blocks, superblocks, and handle repeats

--- blocks.py
+++ blocks.py
@@ -1,43 +1,61 @@
+# TODO: Add param class that simply returns discrete list of params for a given block/elem. Example: param.branchtype.if
 class Block:
     def __init__(self, mod):
         self._mod = mod
+        self._next = None
+        self.ext = "mod"
 
     def __str__(self):
-        return self._mod
+        if self._next:
+            return self._mod + ": %s;" % self._next
+        else:
+            return self._mod + ";"
+
+    def set_next(self, obj):
+        self._next = obj
+
+    def add(self, add_list):
+        self._mod += ":\n\t\t%s, %s, %s" % (str(x) for x in add_list)
 
 
+# todo: determine if element_ids should be passed to block as string or an object
 class Create(Block):
-    def __init__(self, batch_size="", offset_time="", interval="", maximum_batches=""):
-        mod = "CREATE, %s, %s: %s, %s;" % (str(batch_size), str(offset_time), str(interval), str(maximum_batches))
+    def __init__(self, batch_size="", offset_time="", entity_id="", interval="", maximum_batches=""):
+        mod = "CREATE, %s, %s, %s: %s, %s" % (str(batch_size), str(offset_time), str(entity_id), str(interval), str(maximum_batches))
         super().__init__(mod)
 
 
-# todo: implement block label and repeats
-# todo: potentially elements should be a whole other abstract class.
-# todo: Elements may need to contain specific attributes that can be passed to blocks for initialization. They could also be passed in a list for repeats
+# todo: implement balk label
 class Queue(Block):
     def __init__(self, queue_id="", capacity="", balk_label=""):
-        mod = "QUEUE, %s, %s, %s;" % (str(queue_id), str(capacity), str(balk_label))
+        mod = "QUEUE, %s, %s, %s" % (str(queue_id), str(capacity), str(balk_label))
         super().__init__(mod)
 
 
-# todo: handle repeates. Either have add_repeats method or alow for adding lists of resource_id and num_units
 class Seize(Block):
     def __init__(self, priority="", resource_id="", num_units=""):
-        mod = "SEIZE, %s: %s, %s;" % (str(priority), str(resource_id), str(num_units))
+        mod = "SEIZE, %s: %s, %s" % (str(priority), str(resource_id), str(num_units))
         super().__init__(mod)
+
+    def add(self, resource_id="", num_units=""):
+        add_list = [resource_id, num_units]
+        super().add(add_list)
 
 
 class Delay(Block):
     def __init__(self, duration="", storage_id=""):
-        mod = "DELAY: %s, %s;" % (str(duration), str(storage_id))
+        mod = "DELAY: %s, %s" % (str(duration), str(storage_id))
         super().__init__(mod)
 
 
 class Release(Block):
     def __init__(self, resource_id="", quantity_to_release=""):
-        mod = "RELEASE: %s, %s;" % (str(resource_id), str(quantity_to_release))
+        mod = "RELEASE: %s, %s" % (str(resource_id), str(quantity_to_release))
         super().__init__(mod)
+
+    def add(self, resource_id="", quantity_to_release=""):
+        add_list = [resource_id, quantity_to_release]
+        super().add(add_list)
 
 
 class Count(Block):
@@ -48,8 +66,38 @@ class Count(Block):
 
 class Dispose(Block):
     def __init__(self):
-        mod = "DISPOSE;"
+        mod = "DISPOSE"
         super().__init__(mod)
+
+
+# class Assign(Block):
+
+class Branch(Block):
+    def __init__(self, max_branches, rand_num_stream, branch_type, condition, destination_label):
+        mod = "Branch, %s, %s: %s, %s, %s" % (
+            str(x) for x in [max_branches, rand_num_stream, branch_type, condition, destination_label]
+        )
+        super().__init__(mod)
+
+    def add(self, branch_type, condition, destination_label):
+        add_list = [branch_type, condition, destination_label]
+        super().add(add_list)
+
+
+class SuperBlock:
+    """Create group of sequential blocks with reference id"""
+    def __init__(self, super_id, blocks):
+        if not isinstance(blocks, (list, tuple)):
+            raise TypeError('argument must be list or tuple')
+        if len(blocks) == 0:
+            raise ValueError("empty list")
+
+        self.super_id = super_id
+        self.blocks = blocks
+
+    def __str__(self):
+        return self.super_id + "\t\t" + "\n\t\t\t\t".join(self.blocks)
+
 
 c = Create(1)
 print(c)
--- elements.py
+++ elements.py
@@ -3,6 +3,7 @@ class Elements:
     def __init__(self, exp):
         self._exp = exp
         self.elements = []
+        self.ext = "exp"
 
     def __str__(self):
         return self._exp + ";"
@@ -18,9 +19,30 @@ class Project(Elements):
         super().__init__(exp)
 
 
+class Replicate(Elements):
+    def __init__(self, num_replications, begin_time, replication_len, init_system, init_stats, warmup_period):
+        exp = "REPLICATE, %s, %s, %s, %s, %s, %s" % (
+            str(num_replications), str(begin_time), str(replication_len),
+            str(init_system), str(init_stats), str(warmup_period)
+        )
+        super().__init__(exp)
+
+
 class Discrete(Elements):
     def __init__(self, max_entities):
-        exp = "DISCRETE, %s" % max_entities
+        exp = "DISCRETE, %s" % str(max_entities)
+        super().__init__(exp)
+
+
+class Begin(Elements):
+    def __init__(self, listing="", run_controller=""):
+        exp = "BEGIN, %s, %s" % (str(listing), str(run_controller))
+        super().__init__(exp)
+
+
+class End(Elements):
+    def __init__(self):
+        exp = "END"
         super().__init__(exp)
 
 
@@ -42,6 +64,16 @@ class Resources(Elements):
 
     def add(self, number="", name="", capacity=""):
         e = _Resource(number, name, capacity)
+        super().add(e)
+
+
+class Counters(Elements):
+    def __init__(self):
+        exp = "COUNTERS"
+        super().__init__(exp)
+
+    def add(self, number, name, limit, init_option, output_file, report_id):
+        e = _Counter(number, name, limit, init_option, output_file, report_id)
         super().add(e)
 
 
@@ -74,13 +106,24 @@ class _Queue(_Element):
 
 
 class _Resource(_Element):
-    """Resource storage classes"""
+    """Resource storage class"""
     def __init__(self, number="", name="", capacity=""):
         self.number = number
         self.name = name
         self.capacity = capacity
         super().__init__()
 
+
+class _Counter(_Element):
+    """Counter storage class"""
+    def __init__(self, number, name, limit, init_option, output_file, report_id):
+        self.number = number
+        self.name = name
+        self.limit = limit
+        self.init_option = init_option
+        self.output_file = output_file
+        self.report_id = report_id
+        super().__init__()
 
 # todo: include other attributes
 class _Entity(_Element):
