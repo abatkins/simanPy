e6126ef04ec24301b4802b5d23871217fce86252
Alex Atkins <alex04634@gmail.com>
2016-10-04 12:16:07 -0400

    Updated sample

--- examples/sample1.py
+++ examples/sample1.py
@@ -7,20 +7,20 @@ model.add(Project(title="Sample1 Project", analyst_name="Your Name"))
 model.add(Discrete(max_entities=100))
 model.add(Replicate(num_replications=1, begin_time=0, replication_len=480))
 
-entity = Entity()
+entity = Entity(name="Part")
 model.add(CreateBlock(entity, interval="EXPO(4.4)"))
 
-queue = Queue(name="Buffer", ranking_criterion="FIFO")
+queue = Queue(number=1, name="Buffer", ranking_criterion="FIFO")
 model.add(QueueBlock(queue, capacity=100))
 
-resource = Resource(name="Machine")
+resource = Resource(number=1, name="Machine")
 model.add(SeizeBlock(resource))
 
 model.add(DelayBlock("TRIA(3.2,4.2,5.2)"))
 
 model.add(ReleaseBlock(resource))
 
-counter = Counter(name="JobsDone")
+counter = Counter(number=1, name="JobsDone")
 model.add(CountBlock(counter))
 
 model.add(DisposeBlock())
